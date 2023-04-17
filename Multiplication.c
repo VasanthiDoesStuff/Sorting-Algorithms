152a7c3211a28e3794c7b6a6aad22e8b059682df
VB-123 <127195382+VB-123@users.noreply.github.com>
2023-04-18 01:00:03 +0530

    Update Multiplication.c
    Added a few comments

--- Multiplication.c
+++ Multiplication.c
@@ -4,6 +4,7 @@
 int box(int var1, int rows, int columns);
 int multiply(int m, int n);
 
+// taking input
 int main()
 {
     int num1, num2;
@@ -15,29 +16,38 @@ int main()
     return 0;
 }
 
+function to multiply and print the table along with the |
 int multiply(int m, int n)
 {
-    printf("| %2d * %2d = %3d |\n", m, n, m*n);
+    printf("|%2d * %2d = %3d  |\n", m, n, m*n);
     return 0;
 }
 
+\\ size of the top side is taken for alignment
 int n = sizeof("+-----------------+");
 
+\\ function to print the box
 int box(int var1, int rows, int columns)
 {
+    \\ iterate each row
     for (int i = 0; i <= rows; i++)
     {
+        \\ setting a flag variable to break out of loop 
         int printed_result = 0;
+        \\   iterate each coloumn
         for (int j = 0; j < columns; j++)
         {
+            \\ check for corners
             if ((i==0 && j==0) || (i==0 && j==columns-1) || (i==rows && j==0) || (i==rows && j==columns-1))
             {
                 printf("+");
             }
+            \\print the - in the top and bottom
             else if (i==0 || i==rows)
             {
                 printf("-");
             }
+            \\ print the table inside
             else
             {
                 if (i>0 && i<rows && !printed_result)
